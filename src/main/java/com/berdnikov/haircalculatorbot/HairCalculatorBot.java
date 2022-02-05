218c2b0d5a072f96918317332a3c7da73cc335e1
Aleksandr Berdnikov <runer@hot.ee>
2022-02-05 21:59:50 +0200

    initial commit

--- src/main/java/com/berdnikov/haircalculatorbot/HairCalculatorBot.java
+++ src/main/java/com/berdnikov/haircalculatorbot/HairCalculatorBot.java
@@ -46,9 +46,9 @@ public class HairCalculatorBot extends TelegramLongPollingBot {
             sendMsg(message, premium.getPrice(message.getText()));
         }else{
             switch (message.getText()) {
-                case "/start" -> sendMsg(message, "Hello");
-                case "/help" -> sendMsg(message, "dcdc");
-                case "/premium" -> sendMsg(message, premium.getPrice(message.getText()));
+                case "/start" -> sendMsg(message, "Для вычисления наберите /*категория волос* + *длинна волос* + *количество*" +
+                        "\n Категория волос: \n Премиум (/premium или /p ). \nДеткие (/kids или /k )." +
+                        "\n Пример: /p 40 3");
 
             }
         }
